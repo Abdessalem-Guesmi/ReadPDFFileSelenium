0182365e7ea786d6bad59f17d03f7e6299583710
Abdessalem Guesmi <43864053+Abdessalem-Guesmi@users.noreply.github.com>
2021-07-29 23:55:02 +0100

    second commit

--- src/test/java/tests/Test_Case.java
+++ src/test/java/tests/Test_Case.java
@@ -2,34 +2,20 @@ package tests;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import base.BaseTest;
 import utilities.ReadFile;
 
-public class Test_Case {
-	WebDriver driver;
-
-	@BeforeTest
-	public void setUp() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	}
+public class Test_Case extends BaseTest {
 
 	@Test
 	public void verifyContentFile() {
 
-		// please not forgot to change the file location
-		System.out.println("--------please not forgot to change the file location-------");
+		// please don't forgot to change the file location
+		System.out.println("--------please don't forgot to change the file location-------");
 		String url = "file:///D:/TasksTimer%20WKS/ReadPDFFileUsingSelenim/doc/1-Fiche_Prog_JEE.pdf";
 		String path = System.getProperty("user.dir");
 		System.out.println(path);
@@ -46,17 +32,11 @@ public class Test_Case {
 			Assert.assertTrue(pdfContent.contains("- Connaissance en JAVA"));
 			Assert.assertTrue(pdfContent
 					.contains("- Cette formation s'adresse aux développeurs, \r\n" + "concepteurs et chefs de projet"));
-			// features"));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
-	@AfterTest
-	public void tearDown() {
-		driver.quit();
 	}
 
 }
